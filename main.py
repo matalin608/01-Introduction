209030fa01d6205e2aff659d11e9de15f511d76d
Racster <mracster@iu.edu>
2019-08-28 17:05:37 -0400

    commit from class 2

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import utils
 
 utils.check_version((3,7))
 utils.clear()
-
-print('Hello, Class!')
+#Insert question and answer portion for funsies. DELETE THIS COMMENT LATER, DUMMY HEAD
+print('Hello, my name is Matalin Racster!')
+print('My favorite game is Minecraft, of course.')
+print('I have very little experience with code, especially writing code, so I am concerned that I will be confused when working with new coding language in this class.')
+print('though I am concerned, I am also very excited to learn how games work, as well as how computers work. I am excited to combine technical aspects with creativity.')
